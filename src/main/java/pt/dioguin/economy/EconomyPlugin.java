91b7e7ccb68f16fbf66590badde7233f9edf7122
BuildTools <unconfigured@null.spigotmc.org>
2023-01-12 18:45:26 +0000

    new methods in main class

---
+++ src/main/java/pt/dioguin/economy/EconomyPlugin.java
@@ -0,0 +1,22 @@
+package pt.dioguin.economy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EconomyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static EconomyPlugin getInstance(){
+        return getPlugin(EconomyPlugin.class);
+    }
+
+}
